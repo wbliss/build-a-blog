0f1ce74878d9f17fcc8b1031ff0777a8060b310a
Will Bliss <willjbliss@gmail.com>
2017-10-11 00:05:48 -0500

    initial functionality, multi-user access next

---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, flash, session, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'asdjklfakjshdfajhsdfjkadf4345DFeFDAeD'
+
+class Post(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    text = db.Column(db.String(5000))
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+
+@app.route('/posts')
+def posts():
+
+    posts = Post.query.all()
+    return render_template('blogs.html', posts=posts)
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
+    
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+
+        if title != "" and text != "":
+            new_post = Post(title, text)
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect('/posts')
+        else:
+            flash("Please make sure neither field is empty!")
+            return render_template('newpost.html', title=title, text=text)
+
+    
+    return render_template('newpost.html')
+
+if __name__ == '__main__':
+    app.run()
